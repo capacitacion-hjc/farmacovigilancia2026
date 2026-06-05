f401860298e6b8d5a3bd61f59be43c23debe92f7
capacitacion-hjc <ensenanza24@gmail.com>
2026-06-05 11:21:11 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 const API_URL =
-  "https://script.google.com/macros/s/AKfycby-SQfX94R2YvCbE3CHqL2L0cr5fOjDuvlTZZ4OX_mCMZil5mWlxK2vzbLgSOp_Q8Th/exec";
+  "https://script.google.com/macros/s/AKfycbzzk6hdkW5F06qnYZ0wOavPDHaN8s1zGaBQISDkqVhjBpkNdkkb_r0Ydq9c1fmUX4Qw/exec";
 
 document
   .getElementById("registroForm")
