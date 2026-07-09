1ae60f2a40c948fd24af07f5cc4885c54063dfed
capacitacion-hjc <ensenanza24@gmail.com>
2026-07-09 15:28:29 +0000

    UPDATE EDADES

--- dashboard/charts.js
+++ dashboard/charts.js
@@ -153,9 +153,105 @@ function crearGraficaModalidad(datos) {
 }
 
 // ==========================================
+// EDADES
+// ==========================================
+
+function crearGraficaEstados(datos){
+
+    if(graficaEstados){
+        graficaEstados.destroy();
+    }
+
+    const rangos = {
+        "18 - 20": 0,
+        "21 - 30": 0,
+        "31 - 40": 0,
+        "41 - 50": 0,
+        "51 o más": 0
+    };
+
+    datos.forEach(p => {
+
+        const edad = parseInt(p.edad);
+
+        if (isNaN(edad)) return;
+
+        if (edad >= 18 && edad <= 20) {
+            rangos["18 - 20"]++;
+        }
+        else if (edad >= 21 && edad <= 30) {
+            rangos["21 - 30"]++;
+        }
+        else if (edad >= 31 && edad <= 40) {
+            rangos["31 - 40"]++;
+        }
+        else if (edad >= 41 && edad <= 50) {
+            rangos["41 - 50"]++;
+        }
+        else if (edad >= 51) {
+            rangos["51 o más"]++;
+        }
+
+    });
+
+    graficaEstados = new Chart(
+
+        document.getElementById("chartEdades"),
+
+        {
+
+            type: "bar",
+
+            data: {
+
+                labels: Object.keys(rangos),
+
+                datasets: [{
+
+                    label: "Participantes",
+
+                    data: Object.values(rangos),
+
+                    borderWidth: 1,
+                    borderColor: '#13322e',
+                    backgroundColor: '#98989A'
+
+                }]
+
+            },
+
+            options: {
+
+                responsive: true,
+
+                plugins: {
+                    legend: {
+                        display: false
+                    }
+                },
+
+                scales: {
+                    y: {
+                        beginAtZero: true,
+                        ticks: {
+                            precision: 0
+                        }
+                    }
+                }
+
+            }
+
+        }
+
+    );
+
+}
+
+// ==========================================
 // ESTADOS
 // ==========================================
 
+/*
 function crearGraficaEstados(datos) {
 
     if (graficaEstados) {
@@ -221,3 +317,4 @@ function crearGraficaEstados(datos) {
     );
 
 }
+*/
